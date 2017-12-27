ab83c3039bb33dbdc5ec11a9e165e2d127acdd08
tamakiii <tamaki.htm@gmail.com>
2017-12-27 13:38:39 +0900

    s/roles/role/
    Warning: aws_iam_instance_profile.ecs: "roles": [DEPRECATED] Use `role`
    instead. Only a single role can be passed to an IAM Instance Profile

--- lab/iam.tf
+++ lab/iam.tf
@@ -2,13 +2,13 @@
 #   name = "lab-ar-ecs"
 #   assume_role_policy = "${file("policies/assume-role/ecs.json")}"
 # }
-# 
+#
 # resource "aws_iam_policy_attachment" "ecs" {
 #   name = "lab-iam-policy-attach-ecs"
 #   roles = ["${aws_iam_role.ecs.name}"]
 #   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 # }
-# 
+#
 # resource "aws_iam_policy_attachment" "ecs" {
 #   name = "ecs-service-role-attach"
 #   roles = ["${aws_iam_role.ecs.name}"]
@@ -39,7 +39,7 @@ resource "aws_iam_role_policy_attachment" "service" {
 resource "aws_iam_instance_profile" "ecs" {
   name = "lab-ecs-instance-profile"
   path = "/"
-  roles = ["${aws_iam_role.instance.name}"]
+  role = "${aws_iam_role.instance.name}"
 }
 
 
@@ -47,21 +47,21 @@ resource "aws_iam_instance_profile" "ecs" {
 # resource "aws_iam_instance_profile" "service" {
 #   name = "lab-ecs-service-instance-profile"
 #   role = "${aws_iam_role.service.name}"
-# 
+#
 #   depends_on = ["aws_iam_role.service"]
 # }
-# 
+#
 # resource "aws_iam_role_policy" "ecs" {
 #   name = "lab-ecs-policy"
 #   role = "${aws_iam_role.ecs.name}"
 #   policy = "${var.container_iam_role_policy}"
 # }
-# 
+#
 # resource "aws_iam_role_policy_attachment" "instance_policy" {
 #   count = "${length(var.instance_policy_arns)}"
-# 
+#
 #   role       = "${aws_iam_role.app_instance.name}"
 #   policy_arn = "${element(var.instance_policy_arns, count.index)}"
-# 
+#
 #   depends_on = ["aws_iam_role.app_instance"]
 # }
