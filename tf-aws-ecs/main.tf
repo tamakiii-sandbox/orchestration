3b4d671b5d11b9bdb2f6eea38321bab382d5a9a5
tamakiii <tamaki.htm@gmail.com>
2017-12-28 14:11:01 +0900

    add igw, rt, rta

--- tf-aws-ecs/main.tf
+++ tf-aws-ecs/main.tf
@@ -91,3 +91,41 @@ resource "aws_subnet" "charlie" {
 
   depends_on = ["aws_vpc.main"]
 }
+
+resource "aws_internet_gateway" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags {
+    Name = "${var.name}"
+  }
+
+  depends_on = ["aws_vpc.main"]
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.public.id}"
+  }
+
+  tags {
+    Name = "${var.name}"
+  }
+
+  depends_on = ["aws_internet_gateway.public"]
+}
+
+resource "aws_route_table_association" "alpha" {
+  subnet_id = "${aws_subnet.alpha.id}"
+  route_table_id = "${aws_route_table.public.id}"
+
+  depends_on = ["aws_subnet.alpha", "aws_route_table.public"]
+}
+resource "aws_route_table_association" "charlie" {
+  subnet_id = "${aws_subnet.charlie.id}"
+  route_table_id = "${aws_route_table.public.id}"
+
+  depends_on = ["aws_subnet.charlie", "aws_route_table.public"]
+}
