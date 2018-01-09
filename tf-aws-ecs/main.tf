2dbe2aab603aa27642f41c33beb177d3626c8930
tamakiii <tamaki.htm@gmail.com>
2018-01-09 18:09:39 +0900

    add security group

--- tf-aws-ecs/main.tf
+++ tf-aws-ecs/main.tf
@@ -126,3 +126,30 @@ resource "aws_internet_gateway" "public" {
     Name = "${var.name}"
   }
 }
+
+########################################
+# Security group
+########################################
+resource "aws_security_group" "ecs" {
+  vpc_id      = "${aws_vpc.main.id}"
+  name        = "${var.name}-ecs"
+  description = "ECS Allowed Ports"
+
+  ingress {
+    protocol = "tcp"
+    from_port = "80"
+    to_port = "80"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol = -1
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.name}-ecs"
+  }
+}
