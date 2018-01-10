9b1a6e5ceccb091f167a67e5be0f2b7e48e8a8a9
tamakiii <tamaki.htm@gmail.com>
2018-01-10 14:45:26 +0900

    add sg rule for ecs-sg

--- tf-aws-ecs/main.tf
+++ tf-aws-ecs/main.tf
@@ -158,13 +158,6 @@ resource "aws_security_group" "ecs" {
   name        = "${var.name}-ecs"
   description = "ECS Allowed Ports"
 
-  ingress {
-    protocol = "tcp"
-    from_port = 80
-    to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     protocol = -1
     from_port = 0
@@ -175,6 +168,14 @@ resource "aws_security_group" "ecs" {
   tags {
     Name = "${var.name}-ecs"
   }
+}
+resource "aws_security_group_rule" "ecs_dinamic_ports" {
+  security_group_id = "${aws_security_group.ecs.id}"
+  type = "ingress"
+  protocol = "tcp"
+  from_port = 0
+  to_port = 65535
+  source_security_group_id = "${aws_security_group.alb.id}"
 }
 resource "aws_security_group" "alb" {
   vpc_id = "${aws_vpc.main.id}"
