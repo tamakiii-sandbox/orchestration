89cc0764f691795460c58ab2db29eaf9a97f048e
tamakiii <tamaki.htm@gmail.com>
2018-01-10 14:18:39 +0900

    allow ssh

--- tf-aws-ecs/main.tf
+++ tf-aws-ecs/main.tf
@@ -142,6 +142,13 @@ resource "aws_security_group" "ecs" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol = "tcp"
+    from_port = 22
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     protocol = -1
     from_port = 0
