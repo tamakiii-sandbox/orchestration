b95fdc9518488353777202ab564ea0b1aa8458fb
tamakiii <tamaki.htm@gmail.com>
2018-02-16 18:02:55 +0900

    create certification and append it to alb

--- tamakiii.com/output.tf
+++ tamakiii.com/output.tf
@@ -6,5 +6,10 @@ output "overview" {
     - tamakiii.com
       zone_id: ${data.aws_route53_zone.tamakiiicom.zone_id}
 
+  ALB:
+    - ${data.aws_alb.first_run.arn}
+      name: ${data.aws_alb.first_run.name}
+      dns_name: ${data.aws_alb.first_run.dns_name}
+
 EOT
 }
