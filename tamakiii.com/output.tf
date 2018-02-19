54d26a6163e376ce85eaea211ff12cdb66e915bf
tamakiii <tamaki.htm@gmail.com>
2018-02-19 18:22:20 +0900

    rm

--- tamakiii.com/output.tf
+++ tamakiii.com/output.tf
@@ -5,8 +5,6 @@ output "overview" {
   Route53:
     - tamakiii.com
       zone_id: ${data.aws_route53_zone.tamakiiicom.zone_id}
-    - certificate_validation
-      resource_record_name: ${aws_acm_certificate.tamakiiicom.domain_validation_options.0.resource_record_name}
 
   ALB:
     - ${data.aws_alb.first_run.arn}
