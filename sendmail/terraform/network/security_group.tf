332fbed673e9eadce6426ba32be2621376c57fc4
tamakiii <tamaki.htm@gmail.com>
2018-03-15 16:44:14 +0900

    add ingress :8080 to ecs sg

--- sendmail/terraform/network/security_group.tf
+++ sendmail/terraform/network/security_group.tf
@@ -26,6 +26,13 @@ resource "aws_security_group" "ecs" {
   name        = "${var.name}-ecs"
   description = "ECS Allowed Ports"
 
+  ingress {
+    protocol = "tcp"
+    from_port = 8080
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     protocol = -1
     from_port = 0
